6e84920e7975d64d7f7321948901aed515ffb0be
xibau <xibounets@gmail.com>
2021-05-08 23:16:40 +0200

    first commit

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ nickedUser = {}
 nickedReason = {}
 
 
-TOKEN = 'ODQwMzAyMjA0Mjk5OTAyOTk3.YJWOaQ.L-S2WAdU6G4Yux_MRiHa1JSqTeM'
+TOKEN = 'ODQwMzAyMjA0Mjk5OTAyOTk3.YJWOaQ.W6qhix3K8_NNRu8wNv34anSmu7M'
 client = discord.Client()
 
 @client.event
@@ -15,14 +15,14 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-	if message.content.startswith('!autorename'):
-		await message.author.edit(nick=message.content[12:])
-
-@client.event
-async def on_message(message):
 	if message.content.startswith('!rename'):
-		nickedUser = message.mentions[0].id
-		nickReason = message.content[len(str(nickedUser)) + 13:]
-		await message.mentions[0].edit(nick=nickReason)
+		if message.mentions:
+			nickedUser = message.mentions[0].id
+			nickReason = message.content[len(str(nickedUser)) + 13:]
+			await message.mentions[0].edit(nick=nickReason)
+			await message.delete()
+		else :
+			await message.author.edit(nick=message.content[8:])
+			await message.delete()
 
 client.run(TOKEN)
