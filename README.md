fd275b83e0b74c7aa676af0784b4e78bc40f870e
xibau <xibounets@gmail.com>
2022-03-16 00:47:24 +0100

    test

---
+++ README.md
@@ -0,0 +1,11 @@
+# discord-bot
+Discord bot to rename other member with a higher rank using !rename @user nick
+
+You can also rename yourself by just typing !rename nick
+
+The bot need to have a rank higher than the user you wish to rename
+
+WIP : emoji stats to post what are the most used stickers on the server, and delete the ones that are never used. Post when someone delete a sticker, with the username and the sticker's ID
+
+
+
--- bot.py
+++ bot.py
@@ -1,12 +1,16 @@
-
 import os
+from dotenv import load_dotenv
 import discord
 import asyncio
-nickedUser = {}
+import psycopg2
+import cred as cr
+
 nickedReason = {}
+nickedUser = {}
 
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
 
-TOKEN = 'ODQwMzAyMjA0Mjk5OTAyOTk3.YJWOaQ.L-S2WAdU6G4Yux_MRiHa1JSqTeM'
 client = discord.Client()
 
 @client.event
@@ -15,14 +19,25 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-	if message.content.startswith('!autorename'):
-		await message.author.edit(nick=message.content[12:])
-
-@client.event
-async def on_message(message):
 	if message.content.startswith('!rename'):
-		nickedUser = message.mentions[0].id
-		nickReason = message.content[len(str(nickedUser)) + 13:]
-		await message.mentions[0].edit(nick=nickReason)
+		if message.mentions:
+			nickedUser = message.mentions[0].id
+			nickReason = message.content[len(str(nickedUser)) + 12:]
+			await message.mentions[0].edit(nick=nickReason)
+			await message.delete()
+		else :
+			await message.author.edit(nick=message.content[8:])
+			await message.delete()
+
+
+#@client.event
+#async def on_guild_emojis_update(guild, before, after):
+#	channel = client.get_channel(711110467120136254)
+#	async for entry in guild.audit_logs(action=discord.AuditLogAction.emoji_delete, limit=1):
+#		for emoji in before:
+#			if emoji.id != target.id:
+#				continue
+#			else:
+#				await channel.send('{0.user} a supprimé le sticker '.format(entry) + emoji.name)
 
 client.run(TOKEN)
